7fb5221d7eef18e1df0ea1c002b948e93463e9e8
luanalima-Impa <al.luana.lima@impatech.edu.br>
2025-10-30 10:43:15 -0300

    Adição de docstrings

--- src/CB2325NumericaG1/erros/erros.py
+++ src/CB2325NumericaG1/erros/erros.py
@@ -1,5 +1,3 @@
-from decimal import Decimal
-
 def erro_absoluto(valor_real,valor_aprox):
     """Função que calcula o erro absoluto, que corresponde 
     à diferença entre o valor real e o valor aproximado.
@@ -11,8 +9,6 @@ def erro_absoluto(valor_real,valor_aprox):
     Returns:
     Retorna o valor do erro absoluto.
     """
-    erro_absoluto=valor_real-valor_aprox
-    return abs(erro_absoluto)
 
 def erro_relativo(valor_real,valor_aprox):
     '''Função que calcula o erro relativo, que corresponde 
@@ -26,13 +22,3 @@ def erro_relativo(valor_real,valor_aprox):
     Returns:
     Retorna o valor do erro aproximado.
     '''
-    erro_relativo=abs(valor_real-valor_aprox)/valor_real
-    if abs(valor_real-valor_aprox)%valor_real!=0:  # arredonda a divisão não exata
-        erro_relativo = round(erro_relativo, 5) # para 5 casas decimais
-    return abs(erro_relativo)
-
-
-valor_real=Decimal(input())
-valor_aprox=Decimal(input())
-print(erro_absoluto(valor_real, valor_aprox))
-print(erro_relativo(valor_real, valor_aprox))
