5d1332eb02c5d24433b1e6b779c72075ae2aeabd
luanalima-Impa <al.luana.lima@impatech.edu.br>
2025-10-30 10:44:46 -0300

    Implementação das funções erro_relativo e erro_absoluto

--- src/CB2325NumericaG1/erros/erros.py
+++ src/CB2325NumericaG1/erros/erros.py
@@ -9,6 +9,8 @@ def erro_absoluto(valor_real,valor_aprox):
     Returns:
     Retorna o valor do erro absoluto.
     """
+    erro_absoluto=valor_real-valor_aprox
+    return abs(erro_absoluto)
 
 def erro_relativo(valor_real,valor_aprox):
     '''Função que calcula o erro relativo, que corresponde 
@@ -22,3 +24,11 @@ def erro_relativo(valor_real,valor_aprox):
     Returns:
     Retorna o valor do erro aproximado.
     '''
+    erro_relativo=abs(valor_real-valor_aprox)/valor_real
+    return abs(erro_relativo)
+
+
+valor_real=input()
+valor_aprox=input()
+print(erro_absoluto(valor_real, valor_aprox))
+print(erro_relativo(valor_real, valor_aprox))
