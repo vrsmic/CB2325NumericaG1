229dc4888d9652fce157267b0eaa5177d454439e
marianayoshioka <al.mariana.yoshioka@impatech.edu.br>
2025-11-03 10:05:25 -0300

    adicionando teste de hermite

--- tests/test_interpolacao.py
+++ tests/test_interpolacao.py
@@ -1,7 +1,11 @@
-from CB2325NumericaG1.interpolacao import lin_interp
+from CB2325NumericaG1.interpolacao import lin_interp, hermite_interp
 
 def test_lin_interp():
     p = lin_interp([0, 1, 2, 3], [1, 2, 0, 4])
     assert p(1.5) == 1.0
     assert p(2.75) == 3.0
     assert p(0.2) == 1.2
+
+def test_hermite_interp():
+    p = hermite_interp([3, 0], [1, 4], [0, 0])
+    assert p(1.5) == 2.5
