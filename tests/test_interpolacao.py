f145b983b0fcacf2fa8f550ba548467ff61fce11
marianayoshioka <al.mariana.yoshioka@impatech.edu.br>
2025-11-03 09:47:06 -0300

    alterando teste de interpolacao linear

--- tests/test_interpolacao.py
+++ tests/test_interpolacao.py
@@ -3,3 +3,5 @@ from CB2325NumericaG1.interpolacao import lin_interp
 def test_lin_interp():
     p = lin_interp([0, 1, 2, 3], [1, 2, 0, 4])
     assert p(1.5) == 1.0
+    assert p(2.75) == 3.0
+    assert p(0.2) == 1.2
