1f78aa02d19511ca3501ea13eb2239d255783109
Cauan <al.cauan.dutra@impatech.edu.br>
2025-11-08 17:55:29 -0300

    implementação de funções do numpy mais eficientes
    ao invés de dois loops nativos do python.

--- src/CB2325NumericaG1/interpolacao/interpolacao_polinomial.py
+++ src/CB2325NumericaG1/interpolacao/interpolacao_polinomial.py
@@ -6,44 +6,32 @@ def poly_interp(x_val: list,
 
     n = len(x_val)
 
+    x_val_np = np.array(x_val)
+    y_val_np = np.array(y_val)
+
     def P(x):
         acc_som = 0
+
         # loop do somatório
-        for j in range(n):
-            # acumulador
-            acc_prod = 1
-            
+        for j in range(n):          
             # o produtorio exije que j != i
-            x_val_sj = np.delete(x_val, j)
+            x_val_sj = np.delete(x_val_np, j)
 
-            # loop do produtório
-            for i in range(n):
-                if j == i: 
-                    continue
-                acc_prod *= (x - x_val[i])/(x_val[j] - x_val[i])
-            
-            acc_som += y_val[j] * acc_prod
+            # calculando numeradores e denominadores do produtorio
+            num = x - x_val_sj
+            den = x_val_np[j] - x_val_sj
+
+            acc_som += y_val_np[j] * np.prod(num / den)
         
-        resultado = acc_som
-
-        return resultado
+        return acc_som
     
     return P
-
 
 # testes
 
 pol = poly_interp([1, 3, 5], [2, 4, 6])
 
-print(pol(10))
+print(pol(87))
 
 
-# receber pontos (quantos eu quiser?)
-# achar polinomio que passe por todos esses pontos (metodo de lagrange)
-# retornar expressao desse polinomio
-# representar visualmente o polinomio e os pontos dados (talvez colocar um parametro a mais na funcao pra ver se plota ou nao)
-
-
-# existe todo um metodozinho do lagrange pra calcular po
-# vou seguir ele da maneira mais simples, depois vejo como vai ficar a complexidade
-# nah, tenho que fazer com numpy, tinha esquecido
+# tenho que comentar e melhorar o nome das variaveis; codigo precisa estar legivel
