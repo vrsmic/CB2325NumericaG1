5706ad8a8144722145b76e292e4e11253245d203
Luana <al.luana.silva@impatech.edu.br>
2025-11-10 16:20:55 -0300

    Atualizações raízes

--- src/CB2325NumericaG1/raizes/raizes_1.py
+++ src/CB2325NumericaG1/raizes/raizes_1.py
@@ -2,36 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Callable
         
-def bissecao(function: Callable, lower: float, upper: float, tolerance: float, plot = bool True) -> float:
-
+def bissecao(function: Callable, lower: float, upper: float, tolerance: float, plot: bool = True) -> float:
     '''
     Calcula a raiz aproximada de uma função usando o método da bisseção.
 
     =============
 
-    Parâmetros :
+    Parâmetros:
 
     function : Função cuja raiz queremos encontrar, deve aceitar apenas um argumento x.
-
-    lower : Limite inferior do intervalo em que queremos calcular a raiz da funçãp.
-
+    lower : Limite inferior do intervalo em que queremos calcular a raiz da função.
     upper : Limite superior do intervalo em que queremos calcular a raiz da função.
-
-    tolerance : Valor mínimo que o intervalo pode assumir
+    tolerance : Valor mínimo que o intervalo pode assumir.
 
     ============
 
-    Retorna : 
-    - Valor aproximado da raiz da função, arredondado para 3 casas decimais
-
+    Retorna:
+    Valor aproximado da raiz da função, arredondado para 4 casas decimais.
     '''
     
-    # Listas para salvar os dados para o gráfico. 
-    lower_record = []
-    upper_record = []
-    # Adiciona nelas os valores iniciais
-    lower_record.append(lower)
-    upper_record.append(upper)
+    # Listas para salvar os dados para o gráfico
+    lower_record = [lower]
+    upper_record = [upper]
 
     # Avalia a função nos pontos limites do intervalo
     lower_bound = function(lower)
@@ -47,24 +39,22 @@ def bissecao(function: Callable, lower: float, upper: float, tolerance: float, p
     if lower_bound * upper_bound > 0:
         raise ValueError("A função não tem sinais opostos nos limites do intervalo.")
     if tolerance <= 0:
-        raise ValueError("Valor de tolerância inválido")
+        raise ValueError("Valor de tolerância inválido.")
     
-    while upper-lower > tolerance:
-        
-        #Calcula o ponto médio do intervalo
+    while upper - lower > tolerance:
         medium_point = (lower + upper) / 2
         medium_value = function(medium_point)
         
         if medium_value == 0:
             return round(medium_point, 4)
         
-        elif  lower_bound * function(medium_point) < 0:
-
+        if lower_bound * medium_value < 0:
             upper = medium_point
+            upper_bound = medium_value
         else:
             lower = medium_point
+            lower_bound = medium_value
             
-        # Adiciona os novos pontos às listas de histórico
         lower_record.append(lower)
         upper_record.append(upper)
             
@@ -72,28 +62,18 @@ def bissecao(function: Callable, lower: float, upper: float, tolerance: float, p
         x = np.linspace(lower_record[0], upper_record[0], 100)
         y = function(x)
 
-        # Plota o eixo x.
         plt.axhline(0, color='dimgrey', linewidth=1.5)
+        plt.plot(x, y, color='black', linewidth=1.0, label='f(x)') 
 
-        # Plota a função original em preto.
-        plt.plot(x, y, color = 'black', linewidth = 1.0, label = 'f(x)') 
+        plt.scatter(lower_record, function(np.array(lower_record)), s=20, c='royalblue', label='Limite Inferior', zorder=2)
+        plt.scatter(upper_record, function(np.array(upper_record)), s=20, c='crimson', label='Limite Superior', zorder=2)
 
-        lower_points_y = function(np.array(lower_record))
-        upper_points_y = function(np.array(upper_record))
-
-        plt.scatter(lower_record, lower_points_y, s=20.0, c='royalblue', label='Limite Inferior', zorder=2)
         plt.xlim(lower_record[0], upper_record[0])
-        plt.scatter(upper_record, upper_points_y,s=20.0, c='crimson', label='Limite Superior', zorder=2)
-
-
-        # Configuração do gráfico
-        # plt.axis('equal')
         plt.title("Raízes da função pelo Método da Bisseção")
         plt.xlabel("x")
         plt.ylabel("f(x)")
         plt.legend()
         plt.show()
 
-    # Quando o loop para, a função retorna a melhor estimativa, isto é, o ponto médio do último intervalo
     final_root = (lower + upper) / 2
     return round(final_root, 4)
