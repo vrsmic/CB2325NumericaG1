9bb8cfdf20b9555714c292704f0fe29259d16d4a
Alexander <al.alexander.kahleul@impatech.edu.br>
2025-11-10 11:45:40 -0300

    setup para secante

---
+++ src/CB2325NumericaG1/raizes/raizes_3.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Callable
+
+def secante(function: Callable, guess0: float, guess1: float, tolerance: float) -> float:
+    pass
+
