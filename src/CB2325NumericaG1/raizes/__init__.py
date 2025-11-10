20d7f795fb00865a7186c0be1a02cdb2a808bb2c
fragadmsc <al.lucas.damasceno@impatech.edu.br>
2025-11-10 18:22:19 -0300

    correcao dos arquivos init.py (o anterior eu tava na pasta errada)

--- src/CB2325NumericaG1/raizes/__init__.py
+++ src/CB2325NumericaG1/raizes/__init__.py
@@ -1,3 +1,3 @@
 #descomente essas linhas e coloque o nome das funcoes de vcs em func1 e func2
-from .raizes_1 import bissecao
-from .raizes_2 import newton_raphson
+from .raizes_bissecao import bissecao
+from .raizes_newton_raphson import newton_raphson
