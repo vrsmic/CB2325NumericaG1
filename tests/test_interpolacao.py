39d9560d2034b6ba975a7d748179e825fbcbb3fe
marianayoshioka <al.mariana.yoshioka@impatech.edu.br>
2025-11-09 21:06:15 -0300

    adicionei uns testes em interpolacao

--- tests/test_interpolacao.py
+++ tests/test_interpolacao.py
@@ -1,11 +1,17 @@
-from CB2325NumericaG1.interpolacao import lin_interp, hermite_interp
+from CB2325NumericaG1.interpolacao import lin_interp, hermite_interp, poly_interp, vandermond_interp
+from pytest import approx
 
 def test_lin_interp():
     p = lin_interp([0, 1, 2, 3], [1, 2, 0, 4])
-    assert p(1.5) == 1.0
-    assert p(2.75) == 3.0
-    assert p(0.2) == 1.2
+    assert p(1.5) == approx(1.0)
+    assert p(2.75) == approx(3.0)
+    assert p(0.2) == approx(1.2)
 
 def test_hermite_interp():
     p = hermite_interp([3, 0], [1, 4], [0, 0])
-    assert p(1.5) == 2.5
+    assert p(1.5) == approx(2.5)
+
+def test_vandermond_interp():
+    p = vandermond_interp([-2, -1, 0, 1, 2], [4, 1, 0, 1, 4])
+    assert p(3) == approx(9)
+    assert p(2.5) == approx(6.25)
