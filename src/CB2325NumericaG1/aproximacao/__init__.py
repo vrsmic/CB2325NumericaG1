047bf1a9a30111cf31e593003417d536e21b1c8b
fragadmsc <al.lucas.damasceno@impatech.edu.br>
2025-11-12 15:59:42 -0300

    alterado nome dos arquivos de aproximacao

--- src/CB2325NumericaG1/aproximacao/__init__.py
+++ src/CB2325NumericaG1/aproximacao/__init__.py
@@ -1,2 +1,2 @@
-from .aproximacao_1 import regressao, regressao_linear, regressao_logaritmica, polinomio_de_taylor
-from .aproximacao_2 import ajuste_linear
+from .aproximacao_1 import regressao, regressao_logaritmica, polinomio_de_taylor
+from .aproximacao_2 import ajuste_linear, ajuste_trigonometrico
