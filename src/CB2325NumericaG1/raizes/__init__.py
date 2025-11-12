6f9ec6b195090964af11182cb3d358d3956fcee9
fragadmsc <al.lucas.damasceno@impatech.edu.br>
2025-11-12 15:43:03 -0300

    adicionado secante no init.py

--- src/CB2325NumericaG1/raizes/__init__.py
+++ src/CB2325NumericaG1/raizes/__init__.py
@@ -1,3 +1,4 @@
 #descomente essas linhas e coloque o nome das funcoes de vcs em func1 e func2
 from .raizes_bissecao import bissecao
+from .raizes_secante import secante
 from .raizes_newton_raphson import newton_raphson
